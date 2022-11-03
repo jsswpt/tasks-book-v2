6bd939214d750e4bd76346d1fdd2d1553f0b203f
jsswpt <paiusov.d@yandex.ru>
2022-11-03 18:48:36 +0500

    fixed modal

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import ReactDOM from "react-dom";
 
 import st from "./styles.module.scss";
@@ -10,10 +10,12 @@ type ModalProps = {
 };
 
 export const Modal = (props: ModalProps) => {
+  const childrenRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
+
   const el = useMemo(() => {
     const element = document.createElement("div");
     element.id = "modal_wrap";
-    element.className = st.modal;
 
     return element;
   }, []);
@@ -22,12 +24,13 @@ export const Modal = (props: ModalProps) => {
     if (props.isOpen) {
       document.body.appendChild(el);
 
-      el.className = st.modal + " " + st.rising;
+      modalRef.current!.className = st.modal + " " + st.rising;
 
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "scroll";
-      el.className = st.modal + " " + st.hiding;
+      document.body.style.overflow = "auto";
+
+      modalRef.current!.className = st.modal + " " + st.hiding;
 
       const timeOut = setTimeout(() => {
         el.remove();
@@ -36,5 +39,16 @@ export const Modal = (props: ModalProps) => {
     }
   }, [props.isOpen]);
 
-  return ReactDOM.createPortal(props.children, el);
+  return ReactDOM.createPortal(
+    <div className={st.modal} ref={modalRef} onClick={props.onClose}>
+      <div
+        style={{ width: "fit-content" }}
+        ref={childrenRef}
+        onClick={(evt) => evt.stopPropagation()}
+      >
+        {props.children}
+      </div>
+    </div>,
+    el
+  );
 };
--- src/widgets/sidebar/sidebar.tsx
+++ src/widgets/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect, useRef } from "react";
 import { useSidebar } from "entities/sidebar-context/hooks/use-sidebar";
-import React from "react";
 import { Modal } from "shared/ui/modal/modal";
 import st from "./styles.module.scss";
 
@@ -8,17 +8,22 @@ import st from "./styles.module.scss";
 export const Sidebar = React.memo(() => {
   const context = useSidebar();
 
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
   return (
     <div>
       <div className={st.sidebar__xs}>
-        <Modal isOpen={context.isOpen}>
+        <Modal
+          isOpen={context.isOpen}
+          onClose={() => context.toggleIsOpen(false)}
+        >
           <div
+            ref={sidebarRef}
             className={
               context.isOpen
                 ? st.sidebar
                 : `${st.sidebar} ${st.sidebar__hidden}`
             }
-            onClick={() => context.toggleIsOpen(false)}
           >
             sidebar
           </div>
